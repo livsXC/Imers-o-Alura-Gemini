d6ab3ed35a5fe8311319138b8636641e1c5e9fff
Livia Rosario <152235212+livsXC@users.noreply.github.com>
2024-09-06 11:43:49 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+    titulo: "Rayssa Leal",
+    descricao: "Raissa Leal, a Fadinha, é uma skatista brasileira que, com sua habilidade e carisma, revolucionou o esporte. Aos poucos anos de idade, já conquistou o mundo com suas manobras incríveis e se tornou um ícone da nova geração do skate.",
+    link: "https://www.olimpiadas.com.br/atletas/raissa-leal/",
+    tags: "skate fada sk8"
+  },
+  
+   {
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua força e técnica impecável. Com diversas medalhas em competições nacionais e internacionais, ela é um dos maiores nomes do judô brasileiro e inspira jovens atletas a seguirem seus passos.",
+    link: "https://www.cbj.com.br/atletas/beatriz-souza/", // Substitua por um link real para o perfil de Beatriz Souza
+    tags: "judo judoca judô"
+  },
+  
+     {
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o mundo com sua elegância e força. Bicampeã olímpica, ela é um exemplo de superação e dedicação, inspirando milhões de jovens atletas.",
+    link: "https://olympics.com/pt/atletas/rebeca-andrade/",
+    tags: "ginastica ouro"
+  }
+];
